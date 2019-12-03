b2d87ea062b31f809ec8733fe1d1616cd56d18b1
LeoCuantico <leo.kpex@gmail.com>
2019-12-03 10:07:22 -0300

    w

--- contact/models.py
+++ contact/models.py
@@ -2,8 +2,4 @@ from django.db import models
 
 # Create your models here.
 
-"""
-class project(models.Model):
-    title=models.CharField(max_length=100)
-    """
     
--- neutronweb/settings.py
+++ neutronweb/settings.py
@@ -125,10 +125,16 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , "media")
-
+"""
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'leo.kpex@gmail.com'
 EMAIL_HOST_PASSWORD = 'conchetumadre123'
+"""
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "leo.kpex@gmail.com"
+EMAIL_HOST_PASSWORD = 'conchetumadre123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
